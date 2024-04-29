912b8a8edabd02db787ea85959b055802c3b1680
willy <wliu970@gmail.com>
2024-04-29 14:42:44 +0700

    feat: inline server action

--- components/CommentForm.tsx
+++ components/CommentForm.tsx
@@ -1,6 +1,18 @@
-export default function CommentForm({title}) {
+import { createComment } from "@/lib/comments";
+
+export default function CommentForm({slug, title}) {
+  async function action(formData: FormData) {
+    'use server';
+    const message = await createComment({
+      slug,
+      user: formData.get('user'),
+      message: formData.get('message'),
+    });
+    console.log('created:', message);
+  }
+
   return (
-    <form className="border bg-white flex flex-col gap-2 mt-3 px-3 py-2 rounded">
+    <form action={action} className="border bg-white flex flex-col gap-2 mt-3 px-3 py-2 rounded">
       <p className="pb-1">
         Already played <strong>{title}</strong> Have your say!
       </p>
@@ -8,13 +20,13 @@ export default function CommentForm({title}) {
         <label htmlFor="userField" className="shrink-0 w-32">
           Your name
         </label>
-        <input id="userField" className="border px-2 py-1 rounded w-48" />
+        <input id="userField" name="user" className="border px-2 py-1 rounded w-48" />
       </div>
       <div className="flex">
         <label htmlFor="messageField" className="shrink-0 w-32">
           Your comment
         </label>
-        <textarea id="messageField" className="border px-2 py-1 rounded w-full" />
+        <textarea id="messageField" name="message" className="border px-2 py-1 rounded w-full" />
       </div>
       <button type="submit"
         className="bg-orange-800 rounded px-2 py-1 self-center text-slate-50 w-32 hover:bg-orange-700">
